ca459c701393b2180f978566c6e0f63ab750d894
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2021-07-09 20:31:06 +0300

    Add some changes

---
+++ index.js
@@ -0,0 +1,9 @@
+// We remove items form local storage using removeItem('key')
+localStorage.setItem('age', '45')
+localStorage.setItem('name', 'set apart')
+localStorage.setItem('occupation', 'developer')
+localStorage.setItem('name', 'obonyo')
+
+console.log(localStorage.getItem('age'))
+console.log(localStorage.key(1))
+console.log(localStorage)
